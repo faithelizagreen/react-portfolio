e9501cc3913f5a692aa2a9221441ceb9081e449d
faithelizagreen <faithgreen@knights.ucf.edu>
2021-12-03 13:30:18 -0600

    created component file structure and about component

---
+++ src/components/About/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function About () {
+    return (
+        <section className="my-5">
+        <div className="my-2">
+          <div className="profile-img my-5">
+          <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
+          </div>
+          <p>Hello!
+                </p>
+                <p> My name is Faith Green. I'm a former advertising and public relations specialist looking to expand
+                    my career. I love creating and problem solving, and I'm excited to learn all there is to know about
+                    web development.
+                </p>
+                <p>Please keep an eye out for new and exciting projects as I continue to build out my portfolio.</p>
+        </div>
+      </section>
+    );
+}
+
+export default About;
