068c21a93c668e5f937f83739429f7758a4e7888
faithelizagreen <faithgreen@knights.ucf.edu>
2021-12-03 14:12:49 -0600

    created footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,2 +1,39 @@
 import React from 'react'; 
 
+function Footer() {
+    const icons = [
+        {
+            name: 'fab fa-linkedin'
+
+        },
+        {
+            name: 'fab fa-github',
+            link: 'https://github.com/faithelizagreen'
+        },
+        {
+            name: 'fab fa-stack-overflow',
+            link: 'https://stackoverflow.com'
+
+        },
+        {
+            name: 'fab fa-twitter',
+            link: 'twitter.com'
+        },
+        {
+            name: 'fab fa-phone',
+            link: '8507979504'
+        }       
+    ]
+
+    return (
+        <footer className= "flex-row px-1">
+            {icons.map(icon =>
+                (
+                    <a href={icon.link} key={icon.name} target= "_blank"><i className={icon.name}></i></a>
+                    )
+                      )}
+        </footer>
+    );
+}
+
+export default Footer;
